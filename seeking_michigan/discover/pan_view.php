f6a0b3fae9dba2853d0ab6bdad692d93e18d531e
Archives <markharvey@mark-harveys-computer-3.local>
2010-01-29 17:00:56 -0500

    Added comments partial

--- seeking_michigan/discover/pan_view.php
+++ seeking_michigan/discover/pan_view.php
@@ -4,10 +4,6 @@ $rc = dmGetItemInfo($alias, $itnum, $data);
 $parser = xml_parser_create();
 xml_parse_into_struct($parser, $data, $structure, $index);
 xml_parser_free($parser);
-$dmrotate = (isset($_GET["DMROTATE"])) ? $_GET["DMROTATE"] : '0';
-if(isset($structure[$index['TITLE'][0]]["value"])) {
-  $item_title = $structure[$index['TITLE'][0]]["value"];
-}
 ?>
 <div id="item-view">
   <div class="wrapper mod">
@@ -18,11 +14,14 @@ if(isset($structure[$index['TITLE'][0]]["value"])) {
       <h2>Metadata</h2>
       <? display_metadata($alias, $parent_item, $conf); ?>
     <? endif; ?>
+    <? if($_GET['show_comments'] == 1): ?>
+      <? app()->partial('comments', array('alias' => $alias, 'idnum' => $itnum)); ?>
+    <? endif; ?>
   </div>
 </div>
 <div id="sidebar">
   <? prev_next_compound($isthisCompoundObject, $show_all, $previous_item, $next_item, $current_item_num, 
                         $totalitems, $encoded_seek_search_params, $search_position, $alias, $parent_itnum); ?>
   <? prev_next_search($seek_search_params,$search_position); ?>
-  <? partial('buy', $alias, $parent_item); ?>
+  <? # partial('buy', $alias, $parent_item); ?>
 </div>
---
+++ seeking_michigan/include/partials/comments.php
@@ -0,0 +1,1 @@
+<iframe src="http://seekingmichigan.org/contentdm/comments.php?alias=<?= $alias; ?>&amp;idnum=<?= $idnum; ?>" style="width: 100%;"></iframe>
