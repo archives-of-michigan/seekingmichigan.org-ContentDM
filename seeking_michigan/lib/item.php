ab972f714e58b85b89805526c498b6732677fb87
Derek Kastner <dkastner@gmail.com>
2010-02-28 17:50:04 -0500

    finalized Item class

---
+++ seeking_michigan/lib/item.php
@@ -0,0 +1,106 @@
+<?php
+
+class Item {
+  public $alias;
+  public $itnum;
+  public $index;
+  public $title;
+  public $file;
+
+  private $_parent = NULL;
+  private $_collection_path = NULL;
+
+  function __construct($_alias, $_itnum, $parent_compound_object = NULL) {
+    $this->alias = $_alias;
+    $this->itnum = $_itnum;
+
+    if($parent_compound_object) {
+      $this->_parent = $parent_compound_object;
+    }
+  }
+
+  public function parent_item() {
+    if($this->_parent == NULL) {
+      $parent_itnum = GetParent($this->alias, $requested_itnum, $this->collection_path);
+      if($parent_itnum != -1) {
+        $this->_parent = new CompoundObject($this->alias, $parent_itnum);
+      }
+    }
+    return $this->_parent;
+  }
+
+  public function set_parent_item($parent) {
+    $this->_parent = $parent;
+  }
+
+  public function collection_path() {
+    if($this->_collection_path == NULL) {
+      dmGetCollectionParameters($this->alias, $collection_name, $collection_path);
+      $this->_collection_path = $collection_path;
+    }
+    return $this->_collection_path;
+  }
+
+  public function thumbnail_path() {
+    return "/cgi-bin/thumbnail.exe?CISOROOT=".$this->alias."&amp;CISOPTR=".$this->itnum;
+  }
+
+  public function query_string() {
+    if($this->is_child()) {
+      $parent_alias = $this->parent_item()->alias;
+      $parent_itnum = $this->parent_item()->itnum;
+      return "CISOROOT=".$parent_alias."&amp;CISOPTR=".$parent_itnum."&amp;CISOSHOW=".$this->itnum;
+    } else {
+      return "CISOROOT=".$this->alias."&amp;CISOPTR=".$this->itnum;
+    }
+  }
+
+  public function file_type() {
+    return end(explode('.',$this->file));
+  }
+
+  public function is_child() {
+    if($this->parent_item() != -1 && $this->parent_item() != NULL) {
+      return TRUE;
+    } else {
+      return FALSE;
+    }
+  }
+
+  public function is_printable() {
+    if($this->is_child()) {
+      return $this->parent_item()->is_printable();
+    } else if($this->file_type() == 'pdf') {
+      return TRUE;
+    } else {
+      return FALSE;
+    }
+  }
+
+  public function is_pdf() {
+    $file = new SplFileInfo($this->pdf_path());
+    return $file->isFile();
+  }
+
+  public function pdf_path() {
+    return $this->collection_path()."/supp/".$this->itnum."/index.pdf";
+  }
+
+  public function print_link() {
+    if ($this->is_pdf()) {
+      return "/cgi-bin/showfile.exe?CISOROOT=".$this->alias."&amp;CISOPTR=".$this->itnum."&amp;CISOMODE=print";
+    } else {
+      return "print.php?CISOROOT=".$this->alias."&amp;CISOPTR=".$this->itnum;
+    }
+  }
+
+  public static function from_xml($alias, $itnum, $xmlbuffer, $parent = NULL) {
+    $xml = new SimpleXMLElement($xmlbuffer);
+    $doc = ItemFactory::node($xml, '//xml');
+    $item = new Item($alias, $itnum, $parent);
+    $item->title = (string) $doc->title;
+    $item->file = (string) $doc->find;
+
+    return $item;
+  }
+}
---
+++ test/lib/item_test.php
@@ -0,0 +1,135 @@
+<?php
+require_once dirname(__FILE__).'/../test_helper.php';
+require_once dirname(__FILE__).'/../../seeking_michigan/lib/item.php';
+require_once dirname(__FILE__).'/../../seeking_michigan/lib/compound_object.php';
+require_once dirname(__FILE__).'/../../seeking_michigan/lib/item_factory.php';
+
+class ItemTest extends PHPUnit_Framework_TestCase {
+  function setUp() {
+    $this->xmlbuffer = "
+<xml>
+  <title>Adrian (Mich.)</title>
+  <subjec>Adrian High School (Adrian, Mich.); schools</subjec>
+  <descri>High School Building in Adrian (Mich.); c. 1920.</descri>
+  <creato></creato>
+  <date>c. 1920</date>
+  <format>Image</format>
+  <type>Postcard - Color</type>
+  <identi>PH.10458; Town and City Scenes-Adrian-PF.2104</identi>
+  <negati>21568</negati>
+  <source>Part of the Souvenir Folder of Adrian (Mich.)</source>
+  <rights>Use of this image requires the permission of the Archives of Michigan</rights>
+  <order>To order please email archives@mi.gov</order>
+  <featur></featur>
+  <locati></locati>
+  <oclc></oclc>
+  <answer></answer>
+  <ead></ead>
+  <fullrs></fullrs>
+  <find>1608.jp2</find>
+  <dmaccess></dmaccess>
+  <dmimage></dmimage>
+  <dmcreated>2009-08-28</dmcreated>
+  <dmmodified>2009-08-28</dmmodified>
+  <dmoclcno></dmoclcno>
+  <dmrecord>1607</dmrecord>
+</xml>
+    ";
+    $this->item = Item::from_xml('abc123', '3333', $this->xmlbuffer);
+  }
+
+  #thumbnail_path
+  public function testThumbnailPathShouldReturnPathToThumbnail() {
+    $this->assertEquals(
+      "/cgi-bin/thumbnail.exe?CISOROOT=abc123&amp;CISOPTR=3333",
+      $this->item->thumbnail_path());
+  }
+
+  #file_type
+  public function testFileTypeShoudlReturnFileExtension() {
+    $this->assertEquals('jp2', $this->item->file_type());
+  }
+
+  #is_child
+  public function testIsChildShouldReturnTrueIfItemHasParent() {
+    $this->item->set_parent_item(new Item('foo', 'bar'));
+    $this->assertTrue($this->item->is_child());
+  }
+  public function testIsChildShouldReturnFalseIfItemHasNoParent() {
+    $this->assertFalse($this->item->is_child());
+  }
+
+  #collection_path
+  public function testCollectionPathShouldReturnCollectionPath() {
+    $this->assertEquals('D:/foo/bar', $this->item->collection_path());
+  }
+
+  #query_string
+  public function testQueryStringShouldReturnStringWithSubitemIfItemIsAChild() {
+    $co = new CompoundObject('zzzz', '4444');
+    $this->item->set_parent_item($co);
+    $this->assertEquals('CISOROOT=zzzz&amp;CISOPTR=4444&amp;CISOSHOW=3333', $this->item->query_string());
+  }
+  public function testQueryStringShouldReturnStringIfItemIsNotAChild() {
+    $this->assertEquals('CISOROOT=abc123&amp;CISOPTR=3333', $this->item->query_string());
+  }
+
+  #is_printable
+  public function testIsPrintableShouldReturnTrueIfParentIsPrintable() {
+    $co = new CompoundObject('zzzz', '4444');
+    $co->file = "foobar.pdf";
+    $this->item->set_parent_item($co);
+    $this->assertTrue($this->item->is_printable());
+  }
+  public function testIsPrintableShouldReturnFalseIfParentIsNotPrintable() {
+    $co = new CompoundObject('zzzz', '4444');
+    $co->file = "foobar.dat";
+    $this->item->set_parent_item($co);
+    $this->assertFalse($this->item->is_printable());
+  }
+  public function testIsPrintableShouldReturnTrueIfPDF() {
+    $this->item->file = "foobar.pdf";
+    $this->assertTrue($this->item->is_printable());
+  }
+  public function testIsPrintableShouldReturnFalse() {
+    $this->item->file = "foobar.xml";
+    $this->assertFalse($this->item->is_printable());
+  }
+
+  #print_link
+  public function testPrintLinkShouldReturnPDFLink() {
+    $file = $this->getMock('SplFileInfo', array('isFile'), array(), '', false);
+    $file->expects($this->once())->method('isFile')->will($this->returnValue(TRUE));
+    $this->assertEquals(
+      "/cgi-bin/showfile.exe?CISOROOT=abc123&amp;CISOPTR=3333&amp;CISOMODE=print",
+      $this->item->print_link());
+  }
+  public function testPrintLinkShouldReturnURL() {
+    $file = $this->getMock('SplFileInfo', array('isFile'), array(), '', false);
+    $file->expects($this->once())->method('isFile')->will($this->returnValue(FALSE));
+    $this->assertEquals(
+      "print.php?CISOROOT=abc123&amp;CISOPTR=3333",
+      $this->item->print_link());
+  }
+
+  #pdf_path
+  public function testPDFPathShouldReturnPathToPDF() {
+    $this->assertEquals(
+      "D:/foo/bar/supp/3333/index.pdf",
+      $this->item->pdf_path());
+  }
+
+  #from_xml
+  public function testShouldLoadXml() {
+    $item = Item::from_xml('p123456', '1608', $this->xmlbuffer);
+    $this->assertEquals('p123456', $item->alias);
+    $this->assertEquals('1608', $item->itnum);
+    $this->assertEquals('Adrian (Mich.)', $item->title);
+    $this->assertEquals('1608.jp2', $item->file);
+  }
+
+  public function testShouldLoadXmlWithParent() {
+    $item = Item::from_xml('p123456', '1607', $this->xmlbuffer, 'something crazy');
+    $this->assertEquals('something crazy', $item->parent_item());
+  }
+}
