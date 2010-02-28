9061fa07427225baef52d94733cc0fe91ae5e28b
Derek Kastner <dkastner@gmail.com>
2010-02-28 18:54:48 -0500

    removed prev_next_compound script

--- seeking_michigan/lib/helpers/prev_next_compound.php
+++
@@ -1,7 +0,0 @@
-<?php
-if($isthisCompoundObject && !$show_all) {
-  $heading = "Document Pages";
-  $previous_position = $search_position;
-  $next_position = $search_position;
-  include('discover/previous_next.php');
-}
