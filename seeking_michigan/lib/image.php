c93e3140c4b12243fea633adcd8272dd2b048b76
Derek Kastner <dkastner@gmail.com>
2010-02-28 17:50:58 -0500

    finalized Image class

---
+++ seeking_michigan/lib/image.php
@@ -0,0 +1,49 @@
+<?php
+
+class Image extends Item {
+  public static function from_xml($alias, $itnum, $xmlbuffer, $parent = NULL) {
+    $xml = new SimpleXMLElement($xmlbuffer);
+    $doc = ItemFactory::node($xml, '//xml');
+    $image = new Image($alias, $itnum, $parent);
+    $image->title = (string) $doc->title;
+    $image->file = (string) $doc->find;
+
+    return $image;
+  }
+
+  private function load_settings() {
+    if($this->_settings == NULL) {
+      dmGetCollectionImageSettings($alias, $pan_enabled, $minjpegdim, $zoomlevels, $maxderivedimg, $viewer, $docviewer, $compareviewer, $slideshowviewer);
+      dmGetImageInfo($alias, $itnum, $filename, $type, $width, $height);
+      $this->_settings = array('pan_enabled' => $pan_enabled, 'minjpegdim' => $minjpegdim, 
+        'zoomlevels' => $zoomlevels, 'maxderivedimg' => $maxderivedimg, 'viewer' => $viewer, 
+        'docviewer' => $docviewer, 'compareviewer' => $compareviewer, 'slideshowviewer' => $slideshowviewer);
+    }
+  }
+
+  public function width() {
+    $this->load_settings();
+    return $this->_settings['width'];
+  }
+  public function height() {
+    $this->load_settings();
+    return $this->_settings['height'];
+  }
+  public function pan_enabled() {
+    $this->load_settings();
+    return $this->_settings['pan_enabled'];
+  }
+
+  public function is_printable() {
+    return TRUE;
+  }
+
+  public function full_image_path() {
+    $str = "/cgi-bin/getimage.exe?CISOROOT=".$this->alias;
+    $str = $str."&amp;CISOPTR=".$this->itnum;
+    $str = $str."&amp;DMWIDTH=".$this->width();
+    $str = $str."&amp;DMHEIGHT=".$this->height();
+    $str = $str."&amp;DMSCALE=100";
+    return $str;
+  }
+}
---
+++ test/lib/image_test.php
@@ -0,0 +1,64 @@
+<?php
+require_once dirname(__FILE__).'/../test_helper.php';
+require_once dirname(__FILE__).'/../../seeking_michigan/lib/item.php';
+require_once dirname(__FILE__).'/../../seeking_michigan/lib/image.php';
+require_once dirname(__FILE__).'/../../seeking_michigan/lib/item_factory.php';
+
+class ImageTest extends PHPUnit_Framework_TestCase {
+  function setUp() {
+    $this->xmlbuffer = "
+<xml>
+  <title>Adrian (Mich.)</title>
+  <subjec>Adrian High School (Adrian, Mich.); schools</subjec>
+  <descri>High School Building in Adrian (Mich.); c. 1920.</descri>
+  <creato></creato>
+  <date>c. 1920</date>
+  <format>Image</format>
+  <type>Postcard - Color</type>
+  <identi>PH.10458; Town and City Scenes-Adrian-PF.2104</identi>
+  <negati>21568</negati>
+  <source>Part of the Souvenir Folder of Adrian (Mich.)</source>
+  <rights>Use of this image requires the permission of the Archives of Michigan</rights>
+  <order>To order please email archives@mi.gov</order>
+  <featur></featur>
+  <locati></locati>
+  <oclc></oclc>
+  <answer></answer>
+  <ead></ead>
+  <fullrs></fullrs>
+  <find>1608.jp2</find>
+  <dmaccess></dmaccess>
+  <dmimage></dmimage>
+  <dmcreated>2009-08-28</dmcreated>
+  <dmmodified>2009-08-28</dmmodified>
+  <dmoclcno></dmoclcno>
+  <dmrecord>1607</dmrecord>
+</xml>
+    ";
+    $this->image = Image::from_xml('p232323', '1608', $this->xmlbuffer);
+  }
+
+  #full_image_path
+  public function testFullImagePathShouldReturnPathToFullResImage() {
+    $this->image->_settings = array('width' => 640, 'height' => 480);
+
+    $this->assertEquals(
+      "/cgi-bin/getimage.exe?CISOROOT=p232323&amp;CISOPTR=1608&amp;DMWIDTH=640&amp;DMHEIGHT=480&amp;DMSCALE=100",
+      $this->image->full_image_path());
+  }
+
+
+  #from_xml
+  public function testShouldLoadXml() {
+    $image = Image::from_xml('p123456', '1607', $this->xmlbuffer);
+    $this->assertEquals('p123456', $image->alias);
+    $this->assertEquals('1607', $image->itnum);
+    $this->assertEquals('Adrian (Mich.)', $image->title);
+    $this->assertEquals('1608.jp2', $image->file);
+  }
+
+  public function testShouldLoadXmlWithParent() {
+    $image = Image::from_xml('p123456', '1607', $this->xmlbuffer, 'something crazy');
+    $this->assertEquals('something crazy', $image->parent_item());
+  }
+}
