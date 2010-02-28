66a171c637e99a117f3f3a9610b60265f49200a0
Derek Kastner <dkastner@gmail.com>
2010-02-28 18:56:55 -0500

    Updated meta_scr for new Item object

--- seeking_michigan/discover/meta_scr.php
+++ seeking_michigan/discover/meta_scr.php
@@ -1,29 +1,34 @@
 <?
-function display_metadata($alias, $item, $conf) {
-  dmGetCollectionFullResInfo($alias,$enabled,$public,$volprefix,$volsize,$displaysize,$archivesize);
+function display_metadata($item) {
+  dmGetItemInfo($item->alias,$item->itnum,$data);
+  $parser = xml_parser_create();
+  xml_parse_into_struct($parser, $data, $structure, $index);
+  xml_parser_free($parser);
+  $confs = &dmGetCollectionFieldInfo($item->alias);
+  dmGetCollectionFullResInfo($item->alias,$enabled,$public,$volprefix,$volsize,$displaysize,$archivesize);
   $fullres = "";
   $std_fields = array();
   
   
-  for($i = 0; $i < count($conf); $i++) {
-    $tag = strtoupper($conf[$i]["nick"]);
-    if($conf[$i]["type"] != "FTS" && array_key_exists($tag,$item['index']) && 
-       array_key_exists("value",$item['structure'][$item['index'][$tag][0]]) &&
-       $conf[$i]['hide'] != 1) {
-       $value = '';
-       if($conf[$i]["type"] == "DATE") {
-         $value = linkDate($item['structure'][$item['index'][$tag][0]]["value"],$alias,$conf[$i]["nick"]);
-       } else {
-         if(($conf[$i]["search"] == "1") && ($conf[$i]["vocab"] == "1")) {
-           $value = vocabLink(charReplace($item['structure'][$item['index'][$tag][0]]["value"]), $alias, $conf[$i]["nick"]);
-         } elseif(($conf[$i]["search"] == "1") && ($conf[$i]["vocab"] == "0")) {
-           $value = makeLinks(isHyperlink(charReplace($item['structure'][$item['index'][$tag][0]]["value"]),$conf[$i]["type"],$conf[$i]["nick"], $alias));
-         } else {
-           $value = makeLinks(charReplace($item['structure'][$item['index'][$tag][0]]["value"]));
-         }
-       }
-       $std_fields[] = array('field' => $conf[$i]["name"], 'value' => $value);
-     }
+  foreach($confs as $conf) {
+    $tag = strtoupper($conf["nick"]);
+    if($conf["type"] != "FTS" && array_key_exists($tag,$index) && 
+      array_key_exists("value",$structure[$index[$tag][0]]) &&
+      $conf['hide'] != 1) {
+      $value = '';
+      if($conf["type"] == "DATE") {
+        $value = linkDate($structure[$index[$tag][0]]["value"],$item->alias,$conf["nick"]);
+      } else {
+        if(($conf["search"] == "1") && ($conf["vocab"] == "1")) {
+          $value = vocabLink(charReplace($structure[$index[$tag][0]]["value"]), $item->alias, $conf["nick"]);
+        } elseif(($conf["search"] == "1") && ($conf["vocab"] == "0")) {
+          $value = makeLinks(isHyperlink(charReplace($structure[$index[$tag][0]]["value"]),$conf["type"],$conf["nick"], $item->alias));
+        } else {
+          $value = makeLinks(charReplace($structure[$index[$tag][0]]["value"]));
+        }
+      }
+      $std_fields[] = array('field' => $conf["name"], 'value' => $value);
+    }
   }
   
   include('meta_scr_template.php');
