3118603d4e435924388f8ea20ef60ba84e070640
Derek Kastner <dkastner@gmail.com>
2010-03-12 20:34:26 -0500

    completed SearchStatus class for search results pagination within item viewer

--- seeking_michigan/lib/search_status.php
+++ seeking_michigan/lib/search_status.php
@@ -1,40 +1,45 @@
 <?
 class SearchStatus {
-  function __construct($seek_search_params, $search_position) {
-    $this->search_position = $search_position;
-    
-    if(is_string($seek_search_params)) {
-      parse_str($seek_search_params, $query_params);
-    } else {
-      $query_params = $seek_search_params;
-    }
-
-    $search = Search::from_params($query_params);
+  function __construct($seek_search_params) {
+    $search = Search::from_param_string($seek_search_params);
     $search->maxrecs = 3;
-  
-    $prev_item_num = ($this->search_position > 1) ? $this->search_position - 1 : null;
+    $this->search_position = $search->start;
+    $search->start = max(0,$search->start - 1);
     
-    $current_result_index = $this->search_position - 1;
-    $search->start = array(0, max(1, $current_result_index));
+    $this->search_params = $seek_search_params;
+
     $results = $search->results();
     
     if($this->search_position > 1) {
-      $this->previous_item = get_item($results[0]['collection'], $results[0]['pointer']);
-      $this->previous_item['query_string'] .= "&amp;search_position=".($this->search_position - 1);
+      $this->previous_item = $results[0];
     }
+
     if($this->search_position < $search->total) {
-      $next_result_index = ($this->search_position > 1) ? 2 : 1;
-      $this->next_item = get_item($results[$next_result_index]['collection'], $results[$next_result_index]['pointer']);
-      $this->next_item['query_string'] .= "&amp;search_position=".($this->search_position + 1);
+      $this->next_item = $results[2];
     }
   
     $this->total_items = $search->total;
   }
   
+  public $search_params;
+  public $search_position;
   public $previous_item;
   public $next_item;
-  public $search_position;
   public $total_items;
-}
 
-?>
+  public function previous_view_link() {
+    return $this->previous_item->view_link($this->search_params,
+                                           $this->search_position - 1);
+  }
+  public function next_view_link() {
+    return $this->next_item->view_link($this->search_params,
+                                       $this->search_position + 1);
+  }
+
+  public function previous_thumbnail() {
+    return $this->previous_item->thumbnail_path();
+  }
+  public function next_thumbnail() {
+    return $this->next_item->thumbnail_path();
+  }
+}
