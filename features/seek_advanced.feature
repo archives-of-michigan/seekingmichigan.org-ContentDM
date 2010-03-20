75e4bef3768ec1b37c3bda6bb2641c39ef89c4b1
Derek Kastner <dkastner@gmail.com>
2010-03-20 10:53:52 -0400

    adding cucumber

---
+++ features/seek_advanced.feature
@@ -0,0 +1,8 @@
+Feature: Advanced Search
+  As a User
+  I want to search using advanced criteria
+  So that I can better find records
+
+  Scenario: Load the Advanced Search Page
+    When I go to the seek page
+    Then I should see "blarg"
---
+++ features/support/paths.rb
@@ -0,0 +1,23 @@
+module NavigationHelpers
+  # Maps a name to a path. Used by the
+  #
+  #   When /^I go to (.+)$/ do |page_name|
+  #
+  # step definition in web_steps.rb
+  #
+  def path_to(page_name)
+    case page_name
+    when /the seek page/ then
+      '/seek_advanced.php'
+    else
+      if page_name =~ /^\//
+        page_name
+      else
+        raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
+          "Now, go and add a mapping in #{__FILE__}"
+      end
+    end
+  end
+end
+
+World(NavigationHelpers)
